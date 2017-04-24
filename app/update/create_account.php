dcb9708b6f6abd4ccf4a8eb3e540580b8b2e21ba
Bazil Mupisiri <bazilm16@users.noreply.github.com>
2017-04-23 21:16:20 -0500

    changed the post variable names

--- app/update/create_account.php
+++ app/update/create_account.php
@@ -13,17 +13,18 @@ require_once dirname(__FILE__).'/../rsc/classes/User.php';
 $user = new User();
 
 if(isset($_POST["firstname"]) && isset($_POST["lastname"]) && isset($_POST["email"])
-  && isset($_POST["pwd"]) && isset($_POST["sex"])) {
+  && isset($_POST["pwd"]) && isset($_POST["gender"])) {
 
   $user->setFirstName($_POST["firstname"]);
   $user->setLastName($_POST["lastname"]);
   $user->setEmail($_POST["email"]);
   $user->setPassword($_POST["pwd"]);
-  $user->setSex($_POST["sex"]);
+  $user->setSex($_POST["gender"]);
   $user->createAccount();
   $user->setLoadParams(["firstName", "LastName", "email", "dateJoined"]);
   $user->load();
   echo json_encode($user->toArray());
+  header('Location: http://www.luatsu.com');
 } else {
 
 }
