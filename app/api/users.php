b0dcd5917afe06e22f47adf25921c19d17bbdbd6
Bazil Mupisiri <bazilm16@users.noreply.github.com>
2017-05-14 22:05:12 -0500

    the users

--- app/api/users.php
+++ app/api/users.php
@@ -5,11 +5,23 @@
  * Date: 5/14/17
  * Time: 20:21
  */
+require_once '../rsc/classes/Users.php';
+$users = new Users();
 
-require_once '../rsc/classes/Lawyers.php';
+if(isset($_GET["type"]) && isset($_GET["count"])) {
+  //set the type of the users we want to load
+  $users->setType($_GET["type"]);
+  //set the number of users that you want to load
+  $users->setCount((int)$_GET["count"]);
+  //load the lawyers
+  $users->load();
+  //print out the available lawyers
+  echo json_encode(
+    ["status"=>"success", "payload"=>$users->toArray(),"message"=>"none"]
+  );
+} else {
+  echo json_encode(
+    ["status"=>"failure", "payload"=>$users->toArray(),"message"=>"missing one or all of the get parameters"]
+  );
+}
 
-$lawyers = new Lawyers();
-//load the lawyers
-$lawyers->load();
-//print out the available lawyers
-echo json_encode($lawyers->toArray());
