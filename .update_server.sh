555fd377e00e6904de9d65320ed718a13ed6048b
Bazil Mupisiri <bazilm16@users.noreply.github.com>
2017-05-14 23:16:25 -0500

    the update server script

--- .update_server.sh
+++ .update_server.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-git checkout master;git merge backend;git push;ssh mupisiri@luatsu.tech 'cd /var/www/html/luatsu;git pull'
+git checkout master;
+git merge backend;
+git push;
+ssh mupisiri@luatsu.tech 'cd /var/www/html/luatsu;git pull';
+git checkout backend
