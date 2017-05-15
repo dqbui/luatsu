e79ee3b1073b38cde016e69b0456b0b2a8959055
Bazil Mupisiri <bazilm16@users.noreply.github.com>
2017-05-14 22:07:41 -0500

    the users

--- .update_server.sh
+++ .update_server.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-git merge backend;ssh mupisiri@luatsu.tech 'cd /var/www/html/luatsu;git pull'
+git checkout master;
+git merge backend;
+ssh mupisiri@luatsu.tech 'cd /var/www/html/luatsu;git pull';
+git checkout backend
