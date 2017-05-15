97c45467fe3023644b9ffc43ce54a14c86b1e8b3
Bazil Mupisiri <bazilm16@users.noreply.github.com>
2017-05-14 23:36:27 -0500

    updates

--- .update_server.sh
+++ .update_server.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 git checkout master;
 git merge backend;
+git add *;
+git add ./.update_server.sh
+git commit -m "merging the backend branch"
 git push;
 ssh mupisiri@luatsu.tech 'cd /var/www/html/luatsu;git pull';
 git checkout backend;
