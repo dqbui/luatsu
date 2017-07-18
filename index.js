999890b722cad77aa997b5be2d0d19b036572fbf
Linh <phamlinh@grinnell.edu>
2017-07-17 20:39:51 -0500

    Fix instruction

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
+$(document).ready(function() {
+	//YOUR JQUERY CODE
 
-
-
-$(document).ready(function () {
-  //YOUR JQUERY CODE
-  
 	$('#lawyer-picture').click(function(e) {
 		console.log('2test');
-		window.location.href='lawyer_profile_detail.html';
+		window.location.href = 'lawyer_profile_detail.html';
 	});
 
 	$('#lawyer-picture2').click(function(e) {
 		console.log('2test');
-		window.location.href='lawyer_profile_detail.html';
+		window.location.href = 'lawyer_profile_detail.html';
 	});
+
+
+	var appendthis = ("<div class='modal-overlay js-modal-close'></div>");
+
+	$('a[data-modal-id]').click(function(e) {
+		e.preventDefault();
+		$("body").append(appendthis);
+		$(".modal-overlay").fadeTo(500, 0.7);
+		//$(".js-modalbox").fadeIn(500);
+		var modalBox = $(this).attr('data-modal-id');
+		$('#' + modalBox).fadeIn($(this).data());
+	});
+
+
+	$(".js-modal-close, .modal-overlay").click(function() {
+		$(".modal-box, .modal-overlay").fadeOut(500, function() {
+			$(".modal-overlay").remove();
+		});
+	});
+
+	$(window).resize(function() {
+		$(".modal-box").css({
+			top: ($(window).height() - $(".modal-box").outerHeight()) / 2,
+			left: ($(window).width() - $(".modal-box").outerWidth()) / 2
+		});
+	});
+
+	$(window).resize();
+
+
 });
