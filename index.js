0f1223de8f436c00656fdfc66afbcdefab2729e9
Linh <phamlinh@grinnell.edu>
2017-11-21 23:34:05 -0600

    Fix header

--- index.js
+++ index.js
@@ -14,7 +14,15 @@ function div_hide() {
 
 }
 
-
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("menu-header");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
 
 $(document).ready(function() {
 	//YOUR JQUERY CODE
