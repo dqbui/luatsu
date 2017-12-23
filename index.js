ccec6bd79a76538ec26546f9d1966328f3a51b1f
linhpha <phamlinh@grinnell.edu>
2017-12-23 22:39:05 +0700

    Fix header

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ function div_hide() {
 
 }
 
+$("logIn").click(function(){
+    $.get("luatsu.tech/app/api/user.php", function(data, status){
+        alert("Data: " + data + "\nStatus: " + status);
+    });
+});
+
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("menu-header");
